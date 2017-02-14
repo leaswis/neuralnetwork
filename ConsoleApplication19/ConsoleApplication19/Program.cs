633d9b577d44da2aeeace6d79a7b8715151154e1
leaswis <swislaura@gmail.com>
2017-02-15 00:02:04 +0100

    poprawki sciezki+podzialna2metody
    dla kolko i krzyzyk CrossValidXO
    dla danych YT CrossValidYT

--- ConsoleApplication19/ConsoleApplication19/Program.cs
+++ ConsoleApplication19/ConsoleApplication19/Program.cs
@@ -13,13 +13,16 @@ namespace ConsoleApplication19
 
         public static int i = 0;
 
+        public static string path = @"C:\Users\Laura\Desktop\YT_Data.csv";
+
         static void Main(string[] args)
         {
            
             
             List<KeyValuePair<double, double>> csv_results = new List<KeyValuePair<double, double>>();
 
-            int n = CrossValid(csv_results);
+            int n = CrossValidYT(csv_results);
+            //int n = CrossValidXO(csv_results);
  
             var excellent = csv_results.Where(x => Math.Abs(x.Key / x.Value - 1) <= 0.1).Count();
             var passed = csv_results.Where(x => Math.Abs(x.Key / x.Value - 1) > 0.1 && Math.Abs(x.Key / x.Value - 1) <= 0.3).Count();
@@ -42,10 +45,34 @@ namespace ConsoleApplication19
         #region helpers
 
 
-        public static int CrossValid(List<KeyValuePair<double,double>> list_res)
+        public static int CrossValidXO(List<KeyValuePair<double,double>> list_res)
         {
-            var csv_list = CSVRead();
+            var csv_list = CSVReadXO();
  
+
+            foreach (var c in csv_list)
+            {
+                var nn = new NeuralNetwork(2, 1, activate);
+                nn.HiddenLayerCreate(3);
+                nn.HiddenLayerCreate(3);
+
+                TrainingNetwork(ref nn, activate, csv_list.Where(x => x != c).ToList());
+
+                i++;
+
+                var result = nn.ForwardPropagation(c.input);
+
+                list_res.Add(new KeyValuePair<double, double>(result[0, 0], c.output[0]));
+
+            }
+
+            return csv_list.Count();
+        }
+
+        public static int CrossValidYT(List<KeyValuePair<double, double>> list_res)
+        {
+            var csv_list = CSVReadYT();
+
 
             foreach (var c in csv_list)
             {
@@ -75,7 +102,7 @@ namespace ConsoleApplication19
             }
         }
 
-        public static List<TrainingData> CSVRead()
+        public static List<TrainingData> CSVReadYT()
         {
             Console.WriteLine("Wczytanie danych z pliku CSV");
             var list_of_data = new List<TrainingData>();
@@ -90,15 +117,56 @@ namespace ConsoleApplication19
             var column8 = new List<string>();
             var column9 = new List<string>();
             var column10 = new List<string>();
-            /*var column11 = new List<string>();
-            var column12 = new List<string>();
-            var column13 = new List<string>();
-            var column14 = new List<string>();
-            var column15 = new List<string>();*/
+
+            using (var rd = new StreamReader(path))
+            {
+                while (!rd.EndOfStream)
+                {
+                    var splits = rd.ReadLine().Split(';');
+                    column1.Add(splits[0]);
+                    column2.Add(splits[1]);
+                    column3.Add(splits[2]);
+                    column4.Add(splits[3]);
+                    column5.Add(splits[4]);
+
+                }
+            }
 
 
 
-            using (var rd = new StreamReader(@"C:\Users\Laura\Desktop\xo.csv"))
+            for (int i = 0; i < column1.Count(); i++ )
+            {
+                var tr_data = new TrainingData(new double[] { double.Parse(column1[i]), double.Parse(column2[i]), double.Parse(column3[i]), 
+
+                    double.Parse(column4[i])              
+                },
+                    
+                    
+                    new double[] { double.Parse(column5[i]) });
+                list_of_data.Add(tr_data);
+            }
+
+            return list_of_data;
+        }
+
+
+        public static List<TrainingData> CSVReadXO()
+        {
+            Console.WriteLine("Wczytanie danych z pliku CSV");
+            var list_of_data = new List<TrainingData>();
+
+            var column1 = new List<string>();
+            var column2 = new List<string>();
+            var column3 = new List<string>();
+            var column4 = new List<string>();
+            var column5 = new List<string>();
+            var column6 = new List<string>();
+            var column7 = new List<string>();
+            var column8 = new List<string>();
+            var column9 = new List<string>();
+            var column10 = new List<string>();
+
+            using (var rd = new StreamReader(path))
             {
                 while (!rd.EndOfStream)
                 {
@@ -113,30 +181,22 @@ namespace ConsoleApplication19
                     column8.Add(splits[7]);
                     column9.Add(splits[8]);
                     column10.Add(splits[9]);
-                   /* column11.Add(splits[10]);
-                    column12.Add(splits[11]);
-                    column13.Add(splits[12]);
-                    column14.Add(splits[13]);*/
-                    //column15.Add(splits[14]);
-                   
-             
+   
+
                 }
             }
 
 
 
-            for (int i = 0; i < column1.Count(); i++ )
+            for (int i = 0; i < column1.Count(); i++)
             {
                 var tr_data = new TrainingData(new double[] { double.Parse(column1[i]), double.Parse(column2[i]), double.Parse(column3[i]), 
 
                     double.Parse(column4[i]), double.Parse(column5[i]), double.Parse(column6[i]), 
-                    double.Parse(column7[i]), double.Parse(column8[i]), double.Parse(column9[i])//, 
-                   // double.Parse(column10[i]), //double.Parse(column11[i]), double.Parse(column12[i]),
-                  // double.Parse(column13[i])//, double.Parse(column14[i])
-                                  
+                    double.Parse(column7[i]), double.Parse(column8[i]), double.Parse(column9[i])
+                 
                 },
-                    
-                    
+
                     new double[] { double.Parse(column10[i]) });
                 list_of_data.Add(tr_data);
             }
